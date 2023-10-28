89c8cf5e5cf741507d6ef232850514175ce8048c
AurantiacoSucus <30406783+aurantiaco-sucus@users.noreply.github.com>
2023-10-28 18:22:32 +0800

    23.10.28-1

--- net-exp-checksum/src/main.rs
+++ net-exp-checksum/src/main.rs
@@ -1,54 +1,24 @@
 use std::env::args;
 use std::fs::File;
 use std::io::{BufReader, Read};
-use rayon::prelude::*;
-
-fn read_all(path: &str) -> Vec<u16> {
-    let file = File::open(path).unwrap();
-    let size = file.metadata().unwrap().len();
-    let mut file = BufReader::new(file);
-    let mut fc = Vec::with_capacity((size / 2) as usize);
-    let mut buf = [0; 2];
-    while let Ok(n) = file.read(&mut buf) {
-        if n == 0 {
-            break
-        }
-        if n == 1 {
-            buf[1] = 0;
-        }
-        fc.push(u16::from_be_bytes(buf));
-    }
-    fc
-}
-
-fn checksum(data: &[u16]) -> u16 {
-    let mut cur = checksum_pass(data);
-    while cur.len() > 1 {
-        cur = checksum_pass(&cur);
-    }
-    cur[0]
-}
-
-fn checksum_pass(data: &[u16]) -> Vec<u16> {
-    data.chunks(2)
-        .par_bridge()
-        .map(|x| {
-            if x.len() == 1 {
-                x[0]
-            } else {
-                let (mut sum, of) = x[0].overflowing_add(x[1]);
-                if of {
-                    sum += 1;
-                }
-                sum
-            }
-        }).collect::<Vec<_>>()
-}
+use std::time::Instant;
 
 fn main() {
-    let file = args().nth(1)
-        .expect("No input file specified!");
-    let data = read_all(&file);
-    let sum = checksum(&data);
-    println!("{sum:x}");
+    let path = args().nth(1).unwrap();
+    let file = File::open(path).unwrap();
+    let mut file = BufReader::new(file);
+    let begin = Instant::now();
+    let mut buf = [0; 2];
+    let mut sum = 0_u16;
+    while let Ok(n) = file.read(&mut buf) {
+        if n == 0 { break; }
+        if n == 1 { buf[1] = 0; }
+        let val = u16::from_be_bytes(buf);
+        let (mut ns, of) = sum.overflowing_add(val);
+        if of { ns += 1; }
+        sum = ns;
+    }
+    let time = begin.elapsed();
+    println!("Elapse: {time:?}");
+    println!("Checksum: {sum:x}");
 }
